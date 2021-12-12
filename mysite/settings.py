30a8a88fae0b2320b327d6fef1e3f943e29f2832
hinoikari <blackforestant@gmail.com>
2021-12-12 13:26:19 +0100

    Register page and styles add

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'users',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .forms import NewUserForm
+from django.contrib import messages
 
-# Create your views here.
+
 def index(request):
      if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
@@ -30,3 +32,15 @@ def logout_view(request):
     return render(request, "users/login.html", {
                 "message": "Logged Out"
             })
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect(reverse("index"))
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="users/register.html", context={"register_form":form})
